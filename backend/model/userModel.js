561d500f345b7c06fe00dea909de1d90edffc352
aashu shrestha <94774633+AashuShrestha@users.noreply.github.com>
2024-06-17 00:21:15 +0545

    Create userModel.js
    usermodel

---
+++ backend/model/userModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    profileImage: {
+        type: String,
+        default: null,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    emailVerified: {
+        type: Boolean,
+        default: false,
+    },
+    verificationToken: {
+        type: String,
+    },
+    verificationTokenExpires: {
+        type: Date,
+    },
+});
+
+const Users = mongoose.model('User', userSchema);
+module.exports = Users;
